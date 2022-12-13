03daf91770fb5dacb528734feb1a64dfd05f2c25
Parikshit Narang <parikshit3097@gmail.com>
2022-12-12 16:02:55 -0800

    added postgres for saving Student info

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"com.example.student"})
+@EntityScan(basePackages = {"com.example.demo.student"})
+@EnableJpaRepositories(basePackages = {"com.example.demo.student"})
 public class DemoApplication {
     public static void main(String[] args){
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo() {
+        return (args) -> {
+            System.out.println("Done---");
+        };
     }
 }
